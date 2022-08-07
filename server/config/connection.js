d881e94a65d0e5e331c7db0907fb3287c8d71b3e
Camya <camyacodes@gmail.com>
2022-08-07 14:53:33 -0400

    seeded database with fake users

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/indago', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/indago');
 
 module.exports = mongoose.connection;
