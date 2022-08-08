f28f15950b28086147bd3a43612eb06093785281
Camya <camyacodes@gmail.com>
2022-08-08 11:52:58 -0400

    created project model

---
+++ server/models/Project.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose');
+
+const projectSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+        description: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        assigned_users: [
+            {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+            }
+        ],
+        tickets: [
+            {
+              type: Schema.Types.ObjectId,
+              ref: 'Ticket'
+            }
+          ]
+    }
+)
+
+const Project = model('Project', projectSchema)
+
+module.exports = Project;
